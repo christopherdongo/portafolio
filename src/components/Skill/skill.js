8001f797d0ab3e8b8c86dd22e8599af1890ada4d
darkzero <darkzero@localhost.localdomain>
2020-12-17 14:34:52 -0500

    cambios minimos a los skill

--- src/components/Skill/skill.js
+++ src/components/Skill/skill.js
@@ -6,17 +6,18 @@ const Skill=()=>{
 
     return(
         <section className={styles.sectionPrincipal}>
-            <h1>Mis Habilidades</h1>
+            <h1 className={styles.title}>Mis Habilidades</h1>
          <div className={styles.card}>
               { skill.map( (skill, index)=>{
                   return(
                       <article key={index} className={styles.container}>
-                         <picture className={styles.containerIcon}>
+                         <div className={styles.containerIcon}>
                           {skill.icon}
-                         </picture>
-                        {/* <div className={styles.nameSkill}>
+                          <div className={styles.nameSkill}>
                             <h1>{skill.name}</h1>
-                         </div>*/}
+                            <p>{skill.text}</p>
+                         </div>
+                         </div>
                       </article>
                   )
               })}
--- src/components/constants/skill.js
+++ src/components/constants/skill.js
@@ -14,61 +14,80 @@ import Gatsby from "../../skillsvg/gatsby";
 import Gulp from "../../skillsvg/gulp";
 import Github from "../../skillsvg/github";
 import Bash from '../../skillsvg/bash';
+import StyledComponents from '../../skillsvg/styledcomponents'
 
 export default [
   {
     name: "Javascript",
     icon: <Javascript />,
+    text: "Mi lenguaje de programacion preferido"
   },
   {
     name: "Css",
     icon: <Css3 />,
+    text:"me permite dar estilo diseño, color, da vida mis webs"
   },
   {
     name: "Html5",
     icon: <Html5 />,
+    text:"se utiliza para estructurar una pagina web"
   },
   {
     name: "Pug",
     icon: <Pug />,
+    text:"Me permite escribir HTML de una manera mas sencilla"
   },
   {
     name: "ReactJS",
     icon: <ReactSVG />,
+    text:"Libreria escrita en javascript me gusta la facilida de reutilizacion de componentes"
   },
   {
     name: "Gatsby",
     icon: <Gatsby />,
+    text:"permite crear sitios web staticos de una manera rapida y sencilla"
   },
   {
     name: "MongoDB",
     icon: <Mongodb />,
+    text:"La base de datos NOSQL basado en documentos"
   },
   {
     name: "NodeJS",
     icon: <Nodejs />,
+    text:"Permite utilizar javascript del lado del servidor, es muy eficiente las API"
   },
   {
     name: "Bootstrap4",
     icon: <Bootstrap />,
+    text:"Framework de CSS que ayudar en el desarrollo web, especialmente para los sitios responsive "
+  },
+  {
+    name: "Styled Components",
+    icon: <StyledComponents />,
   },
   {
     name: "Sass",
     icon: <Sass />,
+    text:"es un preprocesador css que me permite escribir menos codigo y ahora tiempo en el desarrollo"
   },
   {
     name: "Gulp",
     icon: <Gulp />,
+    text:"es una herramienta, que Me permite automatizar tareas a la hora de realizar mis desarrollos"
   },
   {
     name: "Github",
     icon: <Github />,
+    text:"permite a los desarrollos llevar un control de versiones de nuestros desarrollos de manera eficiente"
   },
   {
     name: "Linux",
     icon: <Linux />,
+    text:"Mi sistema operativo preferido he aprendido mucho de el y sigo en el aprendizaje constante"
   },{
     name:"Bash",
-    icon:<Bash />
+    icon:<Bash />,
+    text:"es lo bueno de linux permite automatizar tareas mediante los archivos Bash"
   }
 ]
