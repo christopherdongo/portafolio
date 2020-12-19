44c8e5435c6c9da11b89bbb282739c4faaf86318
darkzero <darkzero@localhost.localdomain>
2020-12-19 11:30:50 -0500

    cambios minimos descripcion

--- src/components/carousel/newcarousel.js
+++ src/components/carousel/newcarousel.js
@@ -1,34 +1,26 @@
-import React from 'react'
-import Carousel from 'styled-components-carousel';
-import Img from 'gatsby-image' 
+import React from "react"
+import Carousel from "styled-components-carousel"
+import Img from "gatsby-image"
 import styles from "../../css/tenplate.module.css"
 
-const Example = ({Projectimages}) => (
-    <Carousel
-        slidesToShow={1}
-        centerPadding={30}
-        breakpoints={[
-            {
-                size: 200,
-                settings: {
-                    slidesToShow: 1,
-                    showArrows: true,
-                    showIndicator: true,
-                    swipeable: true,
-                    infinite: true,
-                },
-            }
-        ]}
-        
-    >
-     {
-         Projectimages.map((item, index) => (
-            <div key={index} className={styles.Carousel}>
-                <Img fluid={item.fluid} alt="nombre de las imagenes"  className={styles.imagen}/>
-            </div>
-          ))
-     }
-    </Carousel>
-);
+const Example = ({ Projectimages }) => (
+  <Carousel 
+  center 
+  infinite 
+  showArrows 
+  showIndicator 
+  slidesToShow={3}
+  >
+    {Projectimages.map((item, index) => (
+      <div key={index} className={styles.Carousel}>
+        <Img
+          fluid={item.fluid}
+          alt="nombre de las imagenes"
+          className={styles.imagen}
+        />
+      </div>
+    ))}
+  </Carousel>
+)
 
 export default Example
