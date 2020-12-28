e6051a0e0a9bfdace529502773be3758596cff7f
darkzero <darkzero@localhost.localdomain>
2020-12-28 09:51:08 -0500

    host cdn config

--- gatsby-config.js
+++ gatsby-config.js
@@ -36,7 +36,8 @@ module.exports = {
       options: {
         spaceId: `gfkx4627b3pb`,
         // Learn about environment variables: https://gatsby.dev/env-vars
-        accessToken:`4BuRXmFWAlaUFCYCApcd4upDJjLkZUshPIUv35cLj0E`
+        accessToken:`4BuRXmFWAlaUFCYCApcd4upDJjLkZUshPIUv35cLj0E`,
+        host: `preview.contentful.com`,
       }
     },
     {
