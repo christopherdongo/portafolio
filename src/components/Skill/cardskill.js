def227235341a3e20d4fa7fe07730acd74376b09
darkzero <darkzero@localhost.localdomain>
2021-01-03 12:24:09 -0500

    title animation

--- src/components/Skill/cardskill.js
+++ src/components/Skill/cardskill.js
@@ -1,20 +1,23 @@
 import React from "react"
 import loadable from '@loadable/component';
-import { timeout } from 'promise-timeout'  
-import Spinner from '../spinner'
+import { timeout } from 'promise-timeout';  
+import Spinner from '../spinner';
+
 const CardSkill = ({myskill}) => {
 
-   const OtherComponent = loadable(() => timeout(import('./card'),2000), {
+   const OtherComponent = loadable(() => timeout(import('./card'),3000), {
     fallback: <Spinner />,
    } )
 
   return (
     <>
+  
      {
        myskill.map((skill, index) =>(
         <OtherComponent key={index} skill={skill} />
        ))
      }
+
     </>
   )
 }
